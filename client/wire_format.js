ae38aae3f19df0f773b992920bdb1815d1c0620b
Aaron Blankstein <aaron@blockstack.com>
2017-06-08 12:19:33 -0400

    okay, this works in regtest IF the client signs with SIGHASH_ANYONECANPAY | SIGHASH_SINGLE, trying to wrap my head around why SIGHASH_ALLs were getting rejected.

--- client/wire_format.js
+++ client/wire_format.js
@@ -1,14 +1,39 @@
 var request = require('request');
 var bitcoin = require('bitcoinjs-lib');
+var wif = require('wif');
+
+var coreNode = "https://core.blockstack.org";
+var network = bitcoin.networks.bitcoin;
 
 var hash128 = function(buff){
     var ret = Buffer.from(bitcoin.crypto.sha256(buff).slice(0, 16));
-    console.log(ret);
     return ret;
 }
 
+var hex_to_wif = function(hexStr){
+    b = Buffer.from(hexStr, "hex");
+    return wif.encode(network.wif, b, true);
+}
+
+var core_wallet_key_signer = function(wallet){
+    var keySigner = function(unsigned_tx, sigWhat){
+	ownerPrivKey = wallet.owner_privkey
+	wif1 = hex_to_wif(ownerPrivKey.private_keys[0])
+	wif2 = hex_to_wif(ownerPrivKey.private_keys[1])
+	kp1 = bitcoin.ECPair.fromWIF(wif1, network)
+	kp2 = bitcoin.ECPair.fromWIF(wif2, network)
+	redeemScript = Buffer.from(ownerPrivKey.redeem_script, "hex");
+
+	unsigned_tx.sign(0, kp1, redeemScript,
+			 sigWhat);
+	unsigned_tx.sign(0, kp2, redeemScript,
+			 sigWhat);
+    }
+    return keySigner;
+}
+
 var get_latest = function(fqa, cb){
-    request("https://core.blockstack.org/v1/names/" + fqa,
+    request(coreNode + "/v1/names/" + fqa,
 	    function (err, resp, body){
 		if (!err && resp.statusCode == 200) {
 		    cb(JSON.parse(body));
@@ -16,7 +41,16 @@ var get_latest = function(fqa, cb){
 	    });
 }
 
-var make_transfer = function(fqa, consensusHash, newOwner, keyOwner, 
+var get_consensus = function(cb){
+    request(coreNode + "/v1/blockchains/bitcoin/consensus",
+	    function (err, resp, body){
+		if (!err && resp.statusCode == 200) {
+		    cb(JSON.parse(body).consensus_hash);
+		}
+	    });
+}
+
+var make_transfer = function(fqa, consensusHash, newOwner, keySigner, 
 			     keepZonefile, cb){
     // Returns a transfer tx skeleton.
     // this is an unsigned serialized transaction.
@@ -34,43 +68,42 @@ var make_transfer = function(fqa, consensusHash, newOwner, keyOwner,
 
     var op_ret_payload = bitcoin.script.nullData.output.encode(op_ret);
 
-    var tx = new bitcoin.TransactionBuilder()
+    var tx = new bitcoin.TransactionBuilder(network)
 
     tx.addOutput(op_ret_payload, 0);
     tx.addOutput(newOwner, 5500);
     get_latest(fqa, function(latest_info){
 	owner_txid = latest_info.last_txid;
 	owner_addr = latest_info.address;
-	tx.addInput(owner_txid, 5500);
+	console.log(owner_addr);
+	tx.addInput(owner_txid, 1);
 	tx.addOutput(owner_addr, 5500);
-	tx.sign(0, keyOwner, undefined, bitcoin.Transaction.SIGHASH_ANYONECANPAY |
-		bitcoin.Transaction.SIGHASH_ALL)
-	cb(tx.buildIncomplete());
+	keySigner(tx, bitcoin.Transaction.SIGHASH_ANYONECANPAY | bitcoin.Transaction.SIGHASH_SINGLE);
+	cb(tx.build());
     });
 }
 
-keyOwner = bitcoin.ECPair.makeRandom();
 
-make_transfer("ablankstein.id", "5965d05dd8254156662a674c28b8696c",
-	      "34bNQUVgyhSrA8XpM4HkerHUUdNmLpiyj7", keyOwner, false,
-	      function (unsigned_tx) {
-		  console.log(unsigned_tx.toHex());
-	      })
-	      
+var generate_regtest_tx = function(){
+    var fs = require('fs');
+    test_data = JSON.parse(fs.readFileSync('client/test_data.json', 'utf8'));
+    regtest_data = test_data.core_regtest_info;
+    keySigner = core_wallet_key_signer(regtest_data.wallet);
 
-// Inputs:
-// 
-//   Owner scriptSig
-//   Payment scriptSig's
+    get_consensus(function(consensusHash){
+	console.log("consensusHash: " + consensusHash)
+	make_transfer(regtest_data.fqa, consensusHash,
+		      regtest_data.newOwner, keySigner, false,
+		      function(tx){
+			  console.log(tx);
+			  console.log(tx.toHex());
+		      });
+    });
+}
 
-// Outputs:
+//keyOwner = bitcoin.ECPair.makeRandom();
 
-//    OP_RETURN payload
-//    new name owner's scriptPubkey
-//    old name owner's scriptPubkey
-//    payment scriptPubkey change
+coreNode = "http://localhost:6270";
+network = bitcoin.networks.testnet;
 
-/// in core:
-///    -> make unsigned transfer tx -> "transfer_tx" :: returns without subsidy outputs
-///    -> make subsidizable tx -> "tx_make_subsidizable" :: 
-///    -> do tx                -> "do_blockchain_tx"
+generate_regtest_tx();
--- py-test-scripts/handle_txs.py
+++ py-test-scripts/handle_txs.py
@@ -1,24 +1,47 @@
-from blockstack_client.config import get_utxo_provider_client, APPROX_TX_IN_P2SH_LEN
+from blockstack_client.config import get_utxo_provider_client, APPROX_TX_IN_P2SH_LEN, get_tx_broadcaster
 from blockstack_client.operations import fees_transfer
 from blockstack_client.scripts import tx_make_subsidizable
 from blockstack_client.backend.nameops import estimate_payment_bytes
-from blockstack_client.backend.blockchain import get_tx_fee
+from blockstack_client.backend.blockchain import get_tx_fee, broadcast_tx
 from blockstack_client.backend.registrar import get_wallet_payment_privkey_info
 from blockstack_client.tx import deserialize_tx
 from blockstack_client.proxy import get_default_proxy
 from blockstack_client.rpc import local_api_status
 from blockstack_client.actions import get_wallet_with_backoff
 
-import sys
+import sys, os, json
 
-config_path = "/home/aaron/.blockstack/client.ini"
+config_path = os.environ.get("BLOCKSTACK_CLIENT_CONFIG")
+
+def get_wallet():
+    w = {
+	      "data_privkey": "f4c3907cb5769c28ff603c145db7fc39d7d26f69f726f8a7f995a40d3897bb5201", 
+	      "data_pubkey": "046a6582a6566aa4059b7361536e7e4ac3df4d77bf6e843c4c8207eaa12e0ca19e15fc59c959b4a5d6d1de975ab059d9255a795dd57b9c78656a070ea5002efe87", 
+	      "owner_address": "2N41ZaexX9Jq8DRzdJhSs3CE4DGg7K9GmXn", 
+	      "owner_privkey": {
+		  "address": "2N41ZaexX9Jq8DRzdJhSs3CE4DGg7K9GmXn", 
+		  "private_keys": [
+		      "4c3ab2a0704dfd9fdc319cff2c3629b72ebda1580316c7fddf9fad1baa323e9601", 
+		      "75c9f091aa4f0b1544a59e0fce274fb1ac29d7f7e1cd020b66f941e5d260617b01", 
+		      "d62af1329e541871b244c4a3c69459e8666c40b683ffdcb504aa4adc6a559a7701"
+		  ], 
+		  "redeem_script": "522102c2d392595125333ed1d907a236721767a32e7857fcb6783f4749c25989aa3ff321030cdce7e7142157f90bd923e33ec7008eaf42389c2e9e1ed08772a3ef189c32e521034f0e2de5356416258faf2a1f461c2f0e73c2324e8c37488900a5dd3e085fb50153ae"
+	      }, 
+	      "payment_address": "2NCL5euNJV9wNcKWQkTtEv7BxUdSTbaf7W1", 
+	      "payment_privkey": {
+		  "address": "2NCL5euNJV9wNcKWQkTtEv7BxUdSTbaf7W1", 
+		  "private_keys": [
+		      "6f432642c087c2d12749284d841b02421259c4e8178f25b91542c026ae6ced6d01", 
+		      "65268e6267b14eb52dc1ccc500dc2624a6e37d0a98280f3275413eacb1d2915d01", 
+		      "cdabc10f1ff3410082448b708c0f860a948197d55fb612cb328d7a5cc07a6c8a01"
+		  ], 
+		  "redeem_script": "522102d341f728783eb93e6fb5921a1ebe9d149e941de31e403cd69afa2f0f1e698e812102f21b29694df4c2188bee97103d10d017d1865fb40528f25589af9db6e0786b6521028791dc45c049107fb99e673265a38a096536aacdf78aa90710a32fff7750f9f953ae"
+	      }
+    }
+    return w
 
 def subsidize_tx(serialized_tx):
-    if not local_api_status(config_dir="/home/aaron/.blockstack/"):
-        raise Exception("Start api server")
-#    proxy = get_default_proxy(config_path)
-
-    wallet = get_wallet_with_backoff(config_path)
+    wallet = get_wallet()
 
     payment_address = str(wallet["payment_address"])
     payment_privkey_info = wallet["payment_privkey"]
@@ -27,16 +50,13 @@ def subsidize_tx(serialized_tx):
     
     # estimating tx_fee...
     ## will need to pad to estimated length of payment input and output
-    ##   plus owner signature.
 
     num_extra_bytes = estimate_payment_bytes( payment_address, utxo_client, config_path=config_path )
-
     approxed_tx = serialized_tx + '00' * num_extra_bytes
 
     tx_fee = get_tx_fee(approxed_tx, config_path = config_path)
 
-    print tx_fee
-
+    # make the subsidized tx
     subsidized_tx = tx_make_subsidizable(serialized_tx,
                                          fees_transfer,
                                          500000,
@@ -44,12 +64,26 @@ def subsidize_tx(serialized_tx):
                                          utxo_client,
                                          tx_fee=tx_fee)
     
-    return subsidized_tx
+    # broadcast it.
+    try:
+        resp = broadcast_tx(subsidized_tx, config_path = config_path,
+                            tx_broadcaster = get_tx_broadcaster(config_path = config_path))
+    except Exception as e:
+        print('Failed to broadcast transaction: {}'.format(
+            json.dumps(deserialize_tx(subsidized_tx), indent=4)))
+
+        print('raw: \n{}'.format(subsidized_tx))
+        
+        return {'error': 'Failed to broadcast transaction (caught exception)'}
+
+    if 'error' in resp:
+        print('Failed to broadcast transaction: {}'.format(resp['error']))
+
+    return resp
 
 if __name__ == "__main__":
     hex_in = sys.argv[1]
     subsidized = subsidize_tx(hex_in)
     print subsidized
-    print deserialize_tx(subsidized)
 
     
